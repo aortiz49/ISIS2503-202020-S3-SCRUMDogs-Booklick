fc032b76a90a306c1fd971fab72dfe2c7b64c156
aortiz49 <a.ortizg@uniandes.edu.co>
2020-11-19 21:36:28 -0500

    Add GET request for Booklist list as well as DELETE method.

--- models/booklist.py
+++ models/booklist.py
@@ -13,7 +13,7 @@ class BooklistModel(db.Model):
 
     def __init__(self, name: str, description: str, image: str):
         self.name = name
-        self.description= description
+        self.description = description
         self.image = image
 
     def json(self):
