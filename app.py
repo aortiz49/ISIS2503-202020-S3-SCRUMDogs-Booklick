2106c7466bd2fb24df812a52fc1872a04bbe61f7
aortiz49 <a.ortizg@uniandes.edu.co>
2020-12-06 22:17:48 -0500

    Fix databse track.

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from resources.booklist import BookListRegContent, BooklistRegister, BooklistCon
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
